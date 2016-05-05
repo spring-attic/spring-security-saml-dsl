d34fef81660437a910ae6962642d375c4bcf6b93
Jean de Klerk <jadekler@gmail.com>
2016-05-04 21:15:27 -0600

    Issue #2 - provide sample usage with okta

---
+++ samples/spring-security-saml-dsl-sample/src/main/java/com/example/ColombiaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColombiaApplication {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(ColombiaApplication.class, args);
+    }
+
+}
