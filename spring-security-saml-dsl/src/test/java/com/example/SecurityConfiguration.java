d0d03884e47ad5fc89a8d7bbc9c9590a686218a3
Mark Douglass and Mark Kollasch <pair+mark.douglass+mkollasch@gmail.com>
2016-05-06 16:39:36 -0600

    Simplify test configuration structure

--- spring-security-saml-dsl/src/test/java/com/example/SecurityConfiguration.java
+++ spring-security-saml-dsl/src/test/java/com/example/SecurityConfiguration.java
@@ -1,21 +1,23 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.SecurityConfigurer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.extensions.saml2.config.SAMLConfigurer;
 
 import static org.springframework.security.extensions.saml2.config.SAMLConfigurer.saml;
 
 @EnableWebSecurity
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
-@Profile("!http-metadata")
 class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${saml.metadata.path}")
+    private String metadataPath;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         SecurityConfigurer securityConfigurerAdapter =
@@ -26,7 +28,7 @@ class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .keyname("spring")
                     .keyPassword("secret")
                     .and()
-                .metadataFilePath("saml/metadata.xml")
+                .metadataFilePath(metadataPath)
                 .protocol("https")
                 .hostname("localhost:8443")
                 .basePath("/")
