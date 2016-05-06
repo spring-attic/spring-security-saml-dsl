1a2f727dbce0f8b4ff713cc9195ea7c6e6194efd
Mark Douglass and Mark Kollasch <pair+mark.douglass+mkollasch@gmail.com>
2016-05-06 13:58:35 -0600

    Brings back acceptance tests
    This reverts commit 2ec7bb3e8a73f314793360c9cf073b792667127c.

---
+++ spring-security-saml-dsl/src/test/java/acceptance/HttpMetadataLoginTest.java
@@ -0,0 +1,67 @@
+package acceptance;
+
+import com.example.ColombiaApplication;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@WebIntegrationTest
+@SpringApplicationConfiguration(classes = ColombiaApplication.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("test,http-metadata")
+@DirtiesContext(classMode= DirtiesContext.ClassMode.AFTER_CLASS)
+public class HttpMetadataLoginTest {
+    private final WebDriver driver = new FirefoxDriver();
+
+    private int port = 8443;
+    private String baseUrl;
+    private static String username;
+    private static String password;
+
+    @BeforeClass
+    public static void setupClass() throws IOException {
+        Resource resource = new ClassPathResource("credentials.yml");
+        Properties props = new Properties();
+        props.load(resource.getInputStream());
+
+        username = props.getProperty("username");
+        password = props.getProperty("password");
+    }
+
+    @Before
+    public void setup() {
+        baseUrl = String.format("https://localhost:%d", port);
+        driver.get(baseUrl);
+    }
+
+    @After
+    public void teardown() {
+        driver.close();
+    }
+
+    @Test
+    public void canLogin() {
+        driver.findElement(By.name("username")).sendKeys(username);
+        driver.findElement(By.name("password")).sendKeys(password);
+        driver.findElement(By.name("login")).submit();
+
+        assertThat(driver.findElement(By.tagName("body")).getText()).contains("Hello world");
+    }
+}
