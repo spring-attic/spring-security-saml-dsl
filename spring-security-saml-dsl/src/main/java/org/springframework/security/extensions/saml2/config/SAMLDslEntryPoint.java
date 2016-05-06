3afa6ecb5707d89228ba4c74df9997588e2d750c
Rob Winch <rwinch@gopivotal.com>
2016-05-06 13:16:26 -0500

    Repackage DSL

--- spring-security-saml-dsl/src/main/java/org/springframework/security/extensions/saml2/config/SAMLDslEntryPoint.java
+++ spring-security-saml-dsl/src/main/java/org/springframework/security/extensions/saml2/config/SAMLDslEntryPoint.java
@@ -1,4 +1,4 @@
-package com.example.dsl;
+package org.springframework.security.extensions.saml2.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
