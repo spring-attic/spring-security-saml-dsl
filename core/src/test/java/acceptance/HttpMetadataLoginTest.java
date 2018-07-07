d155c3a8e33826b2859eeee7cc742b224586cb63
Patrick Dobler <patrick.dobler@gmail.com>
2018-07-07 14:03:32 +0200

    issue-52: use awaitility's await method instead of sleep

--- core/src/test/java/acceptance/HttpMetadataLoginTest.java
+++ core/src/test/java/acceptance/HttpMetadataLoginTest.java
@@ -1,22 +1,25 @@
 package acceptance;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.springframework.test.context.ActiveProfiles;
 
-import static java.lang.Thread.sleep;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("http-metadata")
 public class HttpMetadataLoginTest extends IntegrationTest {
 
     @Test
-    public void canLogin() throws InterruptedException {
+    public void canLogin() {
         driver.findElement(By.name("username")).sendKeys(username);
         driver.findElement(By.name("password")).sendKeys(password);
         driver.findElement(By.id("okta-signin-submit")).submit();
-        sleep(1000);
+
         System.err.println("Asserting body:"+driver.findElement(By.tagName("body")));
-        assertThat(driver.findElement(By.tagName("body")).getText()).contains("Hello world");
+        await().atMost(5, SECONDS)
+                .untilAsserted(() -> assertThat(driver.findElement(By.tagName("body")).getText()).contains("Hello world"));
     }
 }
