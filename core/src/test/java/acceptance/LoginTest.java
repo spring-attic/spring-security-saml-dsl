111e115fdb05454533298d519e58aed712b39948
Patrick Dobler <patrick.dobler@gmail.com>
2018-07-07 13:01:24 +0200

    issue-52: try an integration test

--- core/src/test/java/acceptance/LoginTest.java
+++ core/src/test/java/acceptance/LoginTest.java
@@ -5,25 +5,40 @@ import org.openqa.selenium.By;
 
 import static java.lang.Thread.sleep;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 public class LoginTest extends IntegrationTest {
 
-    @Test
-    public void canLogin() throws InterruptedException {
-        driver.findElement(By.name("username")).sendKeys(username);
-        driver.findElement(By.name("password")).sendKeys(password);
-        driver.findElement(By.id("okta-signin-submit")).submit();
-        sleep(1000);
+	@Autowired
+	private AuthenticationSuccessHandler authenticationSuccessHandler;
 
-        assertThat(driver.findElement(By.tagName("body")).getText()).contains("Hello world");
-    }
+	@Test
+	public void canLogin() throws InterruptedException, IOException, ServletException {
+		driver.findElement(By.name("username")).sendKeys(username);
+		driver.findElement(By.name("password")).sendKeys(password);
+		driver.findElement(By.id("okta-signin-submit")).submit();
+		sleep(1000);
 
-    @Test
-    public void cantLoginWithBadCreds() throws InterruptedException {
-        driver.findElement(By.name("username")).sendKeys("someguy");
-        driver.findElement(By.name("password")).sendKeys("somepass");
-        driver.findElement(By.id("okta-signin-submit")).submit();
-        sleep(1000);
-        assertThat(driver.findElement(By.tagName("body")).getText()).contains("Sign in failed!");
-    }
+		assertThat(driver.findElement(By.tagName("body")).getText()).contains("Hello world");
+
+		verify(this.authenticationSuccessHandler, times(1)).onAuthenticationSuccess(any(), any(), any());
+	}
+
+	@Test
+	public void cantLoginWithBadCreds() throws InterruptedException {
+		driver.findElement(By.name("username")).sendKeys("someguy");
+		driver.findElement(By.name("password")).sendKeys("somepass");
+		driver.findElement(By.id("okta-signin-submit")).submit();
+		sleep(1000);
+		assertThat(driver.findElement(By.tagName("body")).getText()).contains("Sign in failed!");
+	}
 }
--- core/src/test/java/com/example/SecurityConfiguration.java
+++ core/src/test/java/com/example/SecurityConfiguration.java
@@ -1,12 +1,17 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.SecurityConfigurer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 import static org.springframework.security.extensions.saml2.config.SAMLConfigurer.saml;
 
@@ -17,6 +22,15 @@ class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Value("${saml.metadata.path}")
     private String metadataPath;
+
+    @Autowired
+    ApplicationEventPublisher applicationEventPublisher;
+
+    @SpyBean
+    SimpleUrlAuthenticationSuccessHandler authenticationSuccessHandler;
+
+    @SpyBean
+    SimpleUrlAuthenticationFailureHandler authenticationFailureHandler;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -36,7 +50,10 @@ class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .hostname("localhost:8443")
                 .basePath("/")
                 .entityId("com:example")
-                .and();
+                .and()
+                .successHandler(this.authenticationSuccessHandler)
+                .failureHandler(this.authenticationFailureHandler)
+                .applicationEventPublisher(this.applicationEventPublisher);
 
         http.apply(securityConfigurerAdapter);
 
